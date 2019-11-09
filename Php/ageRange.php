fdb87e0ab895d40b10917f18fd4731ee772de692
Daar375 <Daar375@gmail.com>
2019-11-09 11:27:22 -0600

    Add files via upload

---
+++ Php/ageRange.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Range', 'Users'],
+          ['0-18',     11],
+          ['19-30',      2],
+          ['31-45',  2],
+          ['46-55', 2],
+          ['56-65',    7],
+		  ['66-75',    7],
+		  ['76-85',    7],
+		  ['86+',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
